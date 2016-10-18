4bd14b092951d4114155067edab64943de581b8f
Miao123 <andy@andytekimacbook-air.local>
2016-10-18 11:46:53 +0800

    Initial Commit

---
+++ 任务管理器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  任务管理器
+//
+//  Created by andy on 16/10/18.
+//  Copyright © 2016年 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 任务管理器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  任务管理器
+//
+//  Created by andy on 16/10/18.
+//  Copyright © 2016年 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
